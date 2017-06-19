c3d302751d8241f985930fd1cb5f28af43a11b98
Jacob Luetzow <jacob@devslopes.com>
2017-06-19 14:46:53 -0700

    write and fetch methods for run model done

---
+++ Treads/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Treads
+//
+//  Created by Jacob Luetzow on 6/19/17.
+//  Copyright © 2017 Jacob Luetzow. All rights reserved.
+//
+
+import Foundation
+
+let REALM_QUEUE = DispatchQueue(label: "realmQueue")
