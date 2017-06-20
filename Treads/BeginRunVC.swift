c7305ee6a9bc2e7c00fd2b0a5ea432c499bd15cc
Jacob Luetzow <jacob@devslopes.com>
2017-06-19 19:13:49 -0700

    show last run view added to begin run vc

--- Treads/BeginRunVC.swift
+++ Treads/BeginRunVC.swift
@@ -12,6 +12,12 @@ import MapKit
 class BeginRunVC: LocationVC {
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var lastRunCloseBtn: UIButton!
+    @IBOutlet weak var paceLbl: UILabel!
+    @IBOutlet weak var distanceLbl: UILabel!
+    @IBOutlet weak var durationLbl: UILabel!
+    @IBOutlet weak var lastRunBGView: UIView!
+    @IBOutlet weak var lastRunStack: UIStackView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +28,33 @@ class BeginRunVC: LocationVC {
     override func viewWillAppear(_ animated: Bool) {
         manager?.delegate = self
         manager?.startUpdatingLocation()
+        getLastRun()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         manager?.stopUpdatingLocation()
     }
 
+    func getLastRun() {
+        guard let lastRun = Run.getAllRuns()?.first else {
+            lastRunStack.isHidden = true
+            lastRunBGView.isHidden = true
+            lastRunCloseBtn.isHidden = true
+            return
+        }
+        lastRunStack.isHidden = false
+        lastRunBGView.isHidden = false
+        lastRunCloseBtn.isHidden = false
+        paceLbl.text = lastRun.pace.formatTimeDurationToString()
+        distanceLbl.text = "\(lastRun.distance.metersToMiles(places: 2)) mi"
+        durationLbl.text = lastRun.duration.formatTimeDurationToString()
+    }
+    
+    @IBAction func lastRunCloseBtnPressed(_ sender: Any) {
+        lastRunStack.isHidden = true
+        lastRunBGView.isHidden = true
+        lastRunCloseBtn.isHidden = true
+    }
     
     @IBAction func locationCenterBtnPressed(_ sender: Any) {
         
