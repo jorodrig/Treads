a210b62822a50eedf9145dc912bd8810fe47f3ee
Joseph Rodrigues <joseph@pc-sg.com>
2020-03-06 08:10:12 -0500

    Still working to get the log VC to update on first run

--- Treads/RunLogVC.swift
+++ Treads/RunLogVC.swift
@@ -12,14 +12,19 @@ class RunLogVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    override func viewDidLoad() {
+    override func viewDidAppear() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
     }
+    
+    
 }
 
 extension RunLogVC: UITableViewDelegate, UITableViewDataSource {
+    
+
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return Run.getAllRuns()?.count ?? 0
